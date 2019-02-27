22c4b2a445d603a3eb630dfc55f158afb4b5f742
AndrewOrobator <Orobator.Andrew@gmail.com>
2019-02-26 20:16:40 -0500

    L6: Added syntactic sugar to Espresso test

--- lesson5/src/androidTest/java/com/orobator/helloandroid/lesson5/TipCalcActivityTest2.kt
+++ lesson5/src/androidTest/java/com/orobator/helloandroid/lesson5/TipCalcActivityTest2.kt
@@ -1,7 +1,7 @@
 package com.orobator.helloandroid.lesson5
 
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
@@ -13,8 +13,9 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
+// Testing with Kotlin syntactic sugar
 @RunWith(AndroidJUnit4ClassRunner::class)
-class TipCalcActivityTest {
+class TipCalcActivityTest2 {
   // When creating UI tests, it's helpful to have the XML layout in split view
 
   // The test rule will automatically start our Activity for us
@@ -26,30 +27,28 @@ class TipCalcActivityTest {
   fun whenValidInputsAreReceived_TipIsCalculated() {
     // Calculate Tip
     // Matching to id will always find view regardless of language vs withHint() or withText()
-    onView(withId(R.id.check_amount_EditText))
-        .perform(replaceText("10.00"))
+    R.id.check_amount_EditText typeText "10.00"
 
-    onView(withId(R.id.tip_percent_EditText))
-        .perform(replaceText("25"))
+    R.id.tip_percent_EditText typeText "25"
 
-    onView(withId(R.id.calculate_fab))
-        .perform(click())
+    R.id.calculate_fab.click()
 
     // Assert Inputs Cleared
-    onView(withId(R.id.check_amount_EditText))
-        .check(matches(withText("0.00")))
+    R.id.check_amount_EditText hasText "0.00"
 
-    onView(withId(R.id.tip_percent_EditText))
-        .check(matches(withText("0")))
+    R.id.tip_percent_EditText hasText "0"
 
     // Assert Outputs
-    onView(withId(R.id.check_amount_TextView))
-        .check(matches(withText("$10.00")))
+    R.id.check_amount_TextView hasText "$10.00"
 
-    onView(withId(R.id.tip_amount_TextView))
-        .check(matches(withText("$2.50")))
+    R.id.tip_amount_TextView hasText "$2.50"
 
-    onView(withId(R.id.grand_total_TextView))
-        .check(matches(withText("$12.50")))
+    R.id.grand_total_TextView hasText "$12.50"
   }
 }
+
+infix fun Int.typeText(text: String) = onView(withId(this)).perform(replaceText(text))
+
+fun Int.click() = onView(withId(this)).perform(ViewActions.click())
+
+infix fun Int.hasText(text: String) = onView(withId(this)).check(matches(withText(text)))
